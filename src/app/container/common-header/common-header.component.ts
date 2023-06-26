de955c7eb4abf3b07b5379ecce8a36696d4a037f
Dhruvil Talaviya <Dhruvil@buildesk.in>
2023-06-26 19:14:33 +0530

    common header for download added

---
+++ src/app/container/common-header/common-header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-common-header',
+  templateUrl: './common-header.component.html',
+  styleUrls: ['./common-header.component.scss']
+})
+export class CommonHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  download() {
+    console.log("download called")
+  }
+
+  ngOnInit(): void {
+  }
+}
